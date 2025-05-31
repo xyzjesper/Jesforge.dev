bb4c42a56c85fa5d5f4c9f9f336c86f7f49b299d
Jesper <jesper.l.richert@gmail.com>
2025-05-31 06:48:11 +0200

    fix: correct syntax in vite.config.ts plugins array

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [sveltekit(), tailwindcss(),]
+	plugins: [sveltekit(), tailwindcss()],
 	preview: {
 	    allowedHosts: ["xyzjesper.dev", "jesforge.dev"]
 	}
