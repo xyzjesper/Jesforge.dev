25dbafa86a70fc6bb53f7535c6a111d48759d8d5
Jesper <94371018+xyzjesper@users.noreply.github.com>
2025-06-20 20:28:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
 	preview: {
-	    allowedHosts: ["xyzjesper.dev", "jesforge.dev", "p4oc4ss80kggw0kkc8840s4c.91.99.141.242.sslip.io"]
+	    allowedHosts: ["xyzjesper.dev", "jesforge.dev", "."]
 	}
 });
